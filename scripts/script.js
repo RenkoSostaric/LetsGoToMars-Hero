ca1d2147db4b4052776c0e04495bc75adbc4bf9d
Renko <renkosostaric@gmail.com>
2023-09-12 20:05:10 +0200

    Added question cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
     document.querySelector("#btn-up").addEventListener("click", function() {
-        var $primaryPage = document.querySelector(".page.center");
-        var $secondaryPage = document.querySelector("#page-" + ($primaryPage.getAttribute("id").slice(-1)-1));
-        if($secondaryPage.getAttribute("id").slice(-1) != -1) {
+        if(document.querySelector(".questions-page.center") != null) {
+            $primaryPage = document.querySelector(".questions-page.center");
+            if($primaryPage.getAttribute("id").slice(-1) == 1)
+                $secondaryPage = document.querySelector("#page-0");
+            else
+                $secondaryPage = document.querySelector("#question-page-" + ($primaryPage.getAttribute("id").slice(-1) - 1));
+        } 
+        else if(document.querySelector(".page.center") != null) {
+            $primaryPage = document.querySelector(".page.center");
+            $secondaryPage = document.querySelector("#page-" + ($primaryPage.getAttribute("id").slice(-1) - 1));
+        }
+        console.log($primaryPage);
+        console.log($secondaryPage);
+        if($secondaryPage != null) {
             $($secondaryPage).removeClass("up");
             $($secondaryPage).addClass("center");
             $($primaryPage).removeClass("center");
@@ -11,8 +22,20 @@ $(document).ready(function() {
     });
     document.querySelector("#kartice").addEventListener("click", function() {
         getCountries();
-        var $primaryPage = document.querySelector(".home-page");
-        var $secondaryPage = document.querySelector(".country-page");
+        var $primaryPage = document.querySelector("#page-0");
+        var $secondaryPage = document.querySelector("#page-1");
+        if($($primaryPage).hasClass("down")) {
+            $($primaryPage).removeClass("down");
+        }
+        $($primaryPage).removeClass("center");
+        $($primaryPage).addClass("up");
+        $($secondaryPage).removeClass("down");
+        $($secondaryPage).addClass("center"); 
+    });
+    document.querySelector("#questions").addEventListener("click", function() {
+        getCategoriesQuestions();
+        var $primaryPage = document.querySelector("#page-0");
+        var $secondaryPage = document.querySelector("#question-page-1");
         if($($primaryPage).hasClass("down")) {
             $($primaryPage).removeClass("down");
         }
@@ -31,9 +54,9 @@ function getCountries() {
             var page1 = document.querySelector('#page-1');
             page1.innerHTML = data;
             $(".country-item").click(function(e) {
-                getCategories(this.getAttribute("id").slice(-1));
-                var $primaryPage = document.querySelector(".country-page");
-                var $secondaryPage = document.querySelector(".category-page");
+                getCategories(this.getAttribute("id"));
+                var $primaryPage = document.querySelector("#page-1");
+                var $secondaryPage = document.querySelector("#page-2");
                 if($($primaryPage).hasClass("down")) {
                     $($primaryPage).removeClass("down");
                 }
@@ -55,9 +78,9 @@ function getCategories(id) {
             var page2 = document.querySelector('#page-2');
             page2.innerHTML = data;
             $(".category-item").click(function(e) {
-                getPeople(this.getAttribute("id").slice(-1));
-                var $primaryPage = document.querySelector(".category-page");
-                var $secondaryPage = document.querySelector(".people-page");
+                getPeople(this.getAttribute("id"));
+                var $primaryPage = document.querySelector("#page-2");
+                var $secondaryPage = document.querySelector("#page-3");
                 if($($primaryPage).hasClass("down")) {
                     $($primaryPage).removeClass("down");
                 }
@@ -79,9 +102,9 @@ function getPeople(id) {
             var page3 = document.querySelector('#page-3');
             page3.innerHTML = data;
             $(".person-item").click(function(e) {
-                getPersonInfo(this.getAttribute("id").slice(-1));
-                var $primaryPage = document.querySelector(".people-page");
-                var $secondaryPage = document.querySelector(".info-page");
+                getPersonInfo(this.getAttribute("id"));
+                var $primaryPage = document.querySelector("#page-3");
+                var $secondaryPage = document.querySelector("#page-4");
                 if($($primaryPage).hasClass("down")) {
                     $($primaryPage).removeClass("down");
                 }
@@ -102,6 +125,42 @@ function getPersonInfo(id) {
         success: function(data) {
             var page4 = document.querySelector('#page-4');
             page4.innerHTML = data;
+        }
+    });
+}
+
+function getCategoriesQuestions() {
+    $.ajax({
+        type: "GET",
+        url: "scripts/script.php",
+        data: { function: "getCategoriesQuestions" },
+        success: function(data) {
+            var page1 = document.querySelector('#question-page-1');
+            page1.innerHTML = data;
+            $(".category-item").click(function(e) {
+                getQuestions(this.getAttribute("id"));
+                var $primaryPage = document.querySelector("#question-page-1");
+                var $secondaryPage = document.querySelector("#question-page-2");
+                if($($primaryPage).hasClass("down")) {
+                    $($primaryPage).removeClass("down");
+                }
+                $($primaryPage).removeClass("center");
+                $($primaryPage).addClass("up");
+                $($secondaryPage).removeClass("down");
+                $($secondaryPage).addClass("center"); 
+            });
+        }
+    });
+}
+
+function getQuestions(id) {
+    $.ajax({
+        type: "GET",
+        url: "scripts/script.php",
+        data: { function: "getQuestions", ID_category: id },
+        success: function(data) {
+            var page2 = document.querySelector('#question-page-2');
+            page2.innerHTML = data;
         }
     });
 }
--- scripts/script.php
+++ scripts/script.php
@@ -2,21 +2,27 @@
 $servername = "127.0.0.1";
 $username = "username";
 $password = "password";
-$dbname = "peopleDB";
+$dbname1 = "peopleDB";
+$dbname2 = "questionDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn1 = new mysqli($servername, $username, $password, $dbname1);
+$conn2 = new mysqli($servername, $username, $password, $dbname2);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+if ($conn1->connect_error) {
+    die("Connection failed: " . $conn1->connect_error);
+}
+if ($conn2->connect_error) {
+    die("Connection failed: " . $conn2->connect_error);
 }
 
 function getCountries() {
-    global $conn;
+    global $conn1;
     $sql = "SELECT ID_countries, name, img_src FROM countries";
-    $result = $conn->query($sql);
+    $result = $conn1->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo '<div class="item country-item" id="item-' . $row["ID_countries"] . '">';
+            echo '<div class="item country-item" id="' . $row["ID_countries"] . '">';
             echo '<img src="' . $row["img_src"] . '" alt="">'; 
             echo '<p>' . $row["name"] . '</p>';
             echo '</div>'; 
@@ -28,12 +34,12 @@ function getCountries() {
 
 function getCategories() {
     if (isset($_GET['ID_country'])) {
-        global $conn;
+        global $conn1;
         $sql = "SELECT FK_countries, ID_category, name, img_src FROM category WHERE FK_countries = " . $_GET["ID_country"];
-        $result = $conn->query($sql);
+        $result = $conn1->query($sql);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) { 
-                echo '<div class="item category-item" id="item-' . $row["ID_category"] . '">';
+                echo '<div class="item category-item" id="' . $row["ID_category"] . '">';
                 echo '<img src="' . $row["img_src"] . '" alt="">'; 
                 echo '<p>' . $row["name"] . '</p>';
                 echo '</div>'; 
@@ -47,12 +53,12 @@ function getCategories() {
 }
 
 function getPeople() {
-    global $conn;
+    global $conn1;
     $sql = "SELECT ID_people, name, img_src FROM people WHERE FK_category = " . $_GET["ID_category"];
-    $result = $conn->query($sql);
+    $result = $conn1->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo '<div class="item person-item" id="item-' . $row["ID_people"] . '">';
+            echo '<div class="item person-item" id="' . $row["ID_people"] . '">';
             echo '<img src="' . $row["img_src"] . '" alt="">'; 
             echo '<p>' . $row["name"] . '</p>';
             echo '</div>'; 
@@ -63,14 +69,55 @@ function getPeople() {
 }
 
 function getPersonInfo() {
-    global $conn;
+    global $conn1;
     $sql = "SELECT ID_people, name, img_src, text FROM people WHERE ID_people = " . $_GET["ID_people"];
-    $result = $conn->query($sql);
+    $result = $conn1->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             echo '<div class="info-frame">';
             echo '<h1>' . $row["name"] . '</h1>';
             echo '<p>' . $row["text"] . '</p>';
+            echo '</div>';
+        }
+    } else {
+        echo "0 results" ;
+    }
+}
+
+function getCategoriesQuestions() {
+    global $conn2;
+    $sql = "SELECT ID_category, name, img_src FROM category";
+    $result = $conn2->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="item category-item" id="' . $row["ID_category"] . '">';
+            echo '<img src="' . $row["img_src"] . '" alt="">'; 
+            echo '<p>' . $row["name"] . '</p>';
+            echo '</div>'; 
+        }
+    } else {
+        echo "0 results" ;
+    }
+}
+
+function getQuestions(){
+    global $conn2;
+    $sql = "SELECT ID_card, question, answer_1, answer_2, answer_3, correct_answer, FK_category FROM question_card WHERE FK_category = " . $_GET["ID_category"];
+    $result = $conn2->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="question-item" id="question-' . $row["ID_card"] . '">';
+            echo '<div class="question-box">';
+            echo '<h1 class="question-header">' . $row["question"] . '</h1>';
+            echo '</div>';
+            echo '<div class="question-seperator"></div>';
+            echo '<div class="answer-box">';
+            echo '<ol>';
+            echo '<li class="question-answer ' . ($row["correct_answer"] == 1 ? 'correct-answer' : '') . '" id="answer-1">' . $row["answer_1"] . '</li>';
+            echo '<li class="question-answer ' . ($row["correct_answer"] == 2 ? 'correct-answer' : '') . '" id="answer-2">' . $row["answer_2"] . '</li>';
+            echo '<li class="question-answer ' . ($row["correct_answer"] == 3 ? 'correct-answer' : '') . '" id="answer-3">' . $row["answer_3"] . '</li>';
+            echo '</ol>';
+            echo '</div>';
             echo '</div>';
         }
     } else {
@@ -87,6 +134,10 @@ if (isset($_GET["function"])) {
         getPeople();
     } else if ($_GET["function"] == "getPersonInfo") {
         getPersonInfo();
+    } else if ($_GET["function"] == "getCategoriesQuestions") {
+        getCategoriesQuestions();
+    } else if ($_GET["function"] == "getQuestions") {
+        getQuestions();
     } else {
         echo "Function '" . $_GET["function"] . "' does not exist.";
     }
@@ -94,6 +145,9 @@ if (isset($_GET["function"])) {
     echo "No function specified.";
 }
 
-$conn->close();
+
+
+$conn1->close();
+$conn2->close();
 
 ?>
